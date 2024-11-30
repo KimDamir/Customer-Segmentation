e7d0a5daabc231cbac67725af9764143b5e883d7
Malik <bittiev.malik@mail.ru>
2024-11-30 17:45:34 +0300

    for second dataset

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -4,34 +4,32 @@ from datetime import datetime
 
 def load_and_clean_data(file_path):
     # Load the data
-    df = pd.read_excel(file_path)
+    df = pd.read_csv(file_path)
 
     # Handle missing values in Income by filling with the median
     df['Income'].fillna(df['Income'].median(), inplace=True)
 
     # Convert Dt_Customer to datetime format and calculate customer tenure
-    df['Dt_Customer'] = pd.to_datetime(df['Dt_Customer'])
-    df['Customer_Since'] = (pd.to_datetime('today') - df['Dt_Customer']).dt.days
 
     # Feature engineering: Total children
-    df['Total_Children'] = df['Kidhome'] + df['Teenhome']
 
     # Drop irrelevant columns
-    df.drop(columns=['ID', 'Z_CostContact', 'Z_Revenue', 'Dt_Customer'], inplace=True)
+    df.drop(columns=['ID'], inplace=True)
 
     return df
 
 def encode_and_scale(df):
     # Label Encoding for categorical variables
     le = LabelEncoder()
-    df['Education'] = le.fit_transform(df['Education'])
+    df['Education_level'] = le.fit_transform(df['Education_level'])
     df['Marital_Status'] = le.fit_transform(df['Marital_Status'])
 
     # Scaling numerical features
     scaler = StandardScaler()
-    df[['Income', 'Customer_Since', 'Total_Children', 'MntWines', 'MntFruits', 'MntMeatProducts', 
-        'MntFishProducts', 'MntSweetProducts', 'MntGoldProds']] = scaler.fit_transform(
-        df[['Income', 'Customer_Since', 'Total_Children', 'MntWines', 'MntFruits', 'MntMeatProducts', 
-            'MntFishProducts', 'MntSweetProducts', 'MntGoldProds']])
+    df[['Age', 'Income', 'Total_Spend',
+                    'NumWebPurchases', 'NumCatalogPurchases', 'NumStorePurchases', 'Total_Campaigns_Accepted']] = scaler.fit_transform(
+        df[['Age', 'Income', 'Total_Spend',
+                    'NumWebPurchases', 'NumCatalogPurchases', 'NumStorePurchases', 'Total_Campaigns_Accepted']])
+
 
     return df
